66c8cb39a266980a17b2af6911c5ea0e730a1307
f440 <freq440@gmail.com>
2021-09-24 22:52:26 +0900

    static paths

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,3 +1,5 @@
+import { lstatSync, readdirSync, readFileSync } from "fs";
+import matter from "gray-matter";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { join } from "path";
 import Layout from "../components/layout";
@@ -14,15 +16,67 @@ const Blog = ({ slug }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
 export default Blog;
 
+type Post = { title?: string; path: string; createdAt: string };
+type Posts = Post[];
+
 export const getStaticPaths: GetStaticPaths = async () => {
+  const getFiles = (root: string, files: string[] = []): string[] => {
+    readdirSync(root).forEach((fileName) => {
+      const path = join(root, fileName);
+
+      if (lstatSync(path).isDirectory()) {
+        getFiles(path, files);
+      } else {
+        if (path.match(/\.markdown$/)) {
+          files.push(path);
+        }
+      }
+    });
+
+    return files;
+  };
+
+  const posts: Posts = getFiles(postsDirectory)
+    .map((f) => {
+      const fileContents = readFileSync(f, "utf-8");
+      const { data, content } = matter(fileContents);
+
+      return {
+        path: f,
+        title: data.title,
+        createdAt: data.created_at,
+      };
+    })
+    .sort((a: Post, b: Post) => {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    })
+    .map((post) => {
+      return {
+        ...post,
+        createdAt: post.createdAt.toString(),
+      };
+    });
+
+  const slugs = posts.map((post) => {
+    return post.path
+      .replace(postsDirectory + "/", "")
+      .replace(/(\d{4})-(\d{2})-(\d{2})-(.*)\.markdown$/, "$1/$2/$3/$4");
+  });
+
   return {
-    paths: [{ params: { slug: ["aaa"] } }],
+    paths: slugs.map((slug) => {
+      return {
+        params: { slug: slug.split("/") },
+      };
+    }),
     fallback: false,
   };
 };
 
 export const getStaticProps: GetStaticProps = (context) => {
   return {
-    props: {},
+    props: {
+      slug: context.params?.slug,
+    },
   };
 };
