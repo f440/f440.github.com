68a465f8028aa7598c65f81684a41aa7c9289c14
f440 <freq440@gmail.com>
2021-09-25 20:50:30 +0900

    404

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Custom404: React.VFC = () => {
+  return (
+    <Layout>
+      <article>
+        <h1>Not Found</h1>
+      </article>
+    </Layout>
+  );
+};
+
+export default Custom404;
