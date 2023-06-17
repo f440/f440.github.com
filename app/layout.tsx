adb60b4f4a5209676c4419cab7cbb4d9028a18b0
f440 <freq440@gmail.com>
2023-06-17 21:00:15 +0900

    Move layout component to app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/screen.scss";
-import { Layout } from "../components/layout";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "apatheia.info",
@@ -21,7 +21,28 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <Layout>{children}</Layout>
+        <header>
+          <h1 id="site-title">
+            <Link href="/">apatheia.info</Link>
+          </h1>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>
+          <ul>
+            <li>Link:</li>
+            <li>
+              <a href="https://twitter.com/f440">Twitter</a>
+            </li>
+            <li>
+              <a href="https://github.com/f440">Github</a>
+            </li>
+            <li>
+              <a href="https://pinbaord.in/u:f440">Pinbaord</a>
+            </li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
